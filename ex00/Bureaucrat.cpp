61cea325a605f25c0d5ebfedd3db45535abb5fb5
Candrese <candrese@student.42Heilbronn.de>
2025-06-26 22:51:28 +0200

    added overload output stream operator, exceptions and the functions to change the grade

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,38 +6,84 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 15:49:58 by candrese          #+#    #+#             */
-/*   Updated: 2025/06/04 16:19:11 by candrese         ###   ########.fr       */
+/*   Updated: 2025/06/26 22:47:23 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-
+//d efault constructor
 Bureaucrat::Bureaucrat() : name("Bureaucrat"), grade(150) {}
 
+// parameter constructor for name and grade
 Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name), grade(grade)
-{}
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
 
+// copy constructor
 Bureaucrat::Bureaucrat(const Bureaucrat& other) : name(other.name), grade(other.grade) {}
 
+// assignment operator
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
 {
-	if (this != &other) {
-		name = other.name;
-		grade = other.grade
+	if (this != &other)
+	{
+		grade = other.grade;
 	}
 	return *this;
 }
 
+// destructor
 Bureaucrat::~Bureaucrat()
 {
 	std::cout << name << "destructor called" << std::endl;
 }
 
+// getter grade
 int Bureaucrat::getGrade() const
 {
 	return (grade);
 }
-
-const std::string& Bureaucrat::getName() const {
+// getter name
+const std::string& Bureaucrat::getName() const
+{
 	return (name);
+}
+
+// exception for grade too how
+const char* Bureaucrat::GradeTooHighException::what() const noexcept
+{
+	return ("Grade Too High");
+}
+
+// exception for grade too low
+const char* Bureaucrat::GradeTooLowException::what() const noexcept
+{
+	return ("Grade Too Low");
+}
+
+// increment grade by 1
+void Bureaucrat::incrementGrade()
+{
+	if (grade <= 1)
+		throw GradeTooHighException();
+	grade--;
+}
+
+// decrement grade by 1
+void Bureaucrat::decrementGrade()
+{
+	if (grade >= 150)
+		throw GradeTooLowException();
+	grade++;
+}
+
+// overload output stream operator, printing a bureaucrat
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat)
+{
+	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return (os);
 }
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 15:25:03 by candrese          #+#    #+#             */
-/*   Updated: 2025/06/04 16:20:37 by candrese         ###   ########.fr       */
+/*   Updated: 2025/06/26 22:48:13 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,35 @@
 class Bureaucrat
 {
 private:
-	int grade;
 	const std::string name;
+	int grade;
 
 public:
 	Bureaucrat();
+	Bureaucrat(const std::string& name, int grade);
 	Bureaucrat(const Bureaucrat &src);
 	Bureaucrat &operator=(const Bureaucrat &src);
 	~Bureaucrat();
 
-	int getGrade();
+	int getGrade() const;
 	const std::string& getName() const;
 
 	void incrementGrade();
 	void decrementGrade();
+
+	class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+		
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
 };
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& src);
 
 #endif
