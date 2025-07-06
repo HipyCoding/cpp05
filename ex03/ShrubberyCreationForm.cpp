b9e9cdcb559e7f3c6a4b7e3b4f675ef5140938a4
Christian Andresen <christian@Mac.fritz.box>
2025-07-06 22:45:53 +0200

    copy paste from previous

---
+++ ex03/ShrubberyCreationForm.cpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: christian <christian@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 03:52:10 by christian         #+#    #+#             */
+/*   Updated: 2025/07/03 04:12:42 by christian        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ShrubberyCreationForm.hpp"
+
+// default constructor
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm", 145, 137), target("default")
+{}
+
+// parameter constructor
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target) 
+	: AForm("ShrubberyCreationForm", 145, 137), target(target)
+{}
+
+// copy constructor
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other)
+	: AForm(other), target(other.target)
+{}
+
+// assignment operator
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
+{
+	if (this != &other)
+	{
+		AForm::operator=(other);
+		target = other.target;
+	}
+	return *this;
+}
+
+// destructor
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+	std::cout << "ShrubberyCreationForm destructor called" << std::endl;
+}
+
+// execute function that calls parent execute for checks
+void ShrubberyCreationForm::execute(Bureaucrat const & executor) const
+{
+	AForm::execute(executor);
+}
+
+// actual action implementation
+void ShrubberyCreationForm::executeAction() const
+{
+	std::string filename = target + "_shrubbery";
+	std::ofstream file(filename.c_str());
+	
+	if (!file.is_open())
+	{
+		std::cerr << "Error: could not create file " << filename << std::endl;
+		return;
+	}
+	
+	file << "        /\\        " << std::endl;
+	file << "       /  \\       " << std::endl;
+	file << "      /    \\      " << std::endl;
+	file << "     /______\\     " << std::endl;
+	file << "         ||        " << std::endl;
+	file << "         ||        " << std::endl;
+	file << "                   " << std::endl;
+	file << "    /\\    /\\       " << std::endl;
+	file << "   /  \\  /  \\      " << std::endl;
+	file << "  /    \\/    \\     " << std::endl;
+	file << " /______________\\   " << std::endl;
+	file << "       ||||         " << std::endl;
+	file << "       ||||         " << std::endl;
+	file << "                   " << std::endl;
+	file << "      /\\           " << std::endl;
+	file << "     /  \\          " << std::endl;
+	file << "    /    \\         " << std::endl;
+	file << "   /______\\        " << std::endl;
+	file << "       ||           " << std::endl;
+	file << "       ||           " << std::endl;
+	
+	file.close();
+	std::cout << "ascii trees created in " << filename << std::endl;
+}
---
+++ ex03/ShrubberyCreationForm.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: christian <christian@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 03:52:13 by christian         #+#    #+#             */
+/*   Updated: 2025/07/03 04:12:22 by christian        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+#include "AForm.hpp"
+#include <fstream>
+
+class ShrubberyCreationForm : public AForm
+{
+private:
+	std::string target;
+
+public:
+	ShrubberyCreationForm();
+	ShrubberyCreationForm(const std::string& target);
+	ShrubberyCreationForm(const ShrubberyCreationForm& other);
+	ShrubberyCreationForm& operator=(const ShrubberyCreationForm& other);
+	~ShrubberyCreationForm();
+
+	void execute(Bureaucrat const & executor) const override;
+
+protected:
+	void executeAction() const override;
+};
+
+#endif
