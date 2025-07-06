d8504aa60eca22f67d2b8da665097b98865f40bb
Christian Andresen <christian@Mac.fritz.box>
2025-07-06 22:39:12 +0200

    bureaucrat class similar to last exercise

---
+++ ex02/Bureaucrat.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: christian <christian@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 20:51:47 by candrese          #+#    #+#             */
+/*   Updated: 2025/07/03 04:11:44 by christian        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include "AForm.hpp"
+# include <iostream>
+# include <string>
+# include <stdexcept>
+
+class AForm;
+
+class Bureaucrat
+{
+private:
+	const std::string name;
+	int grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string& name, int grade);
+	Bureaucrat(const Bureaucrat &src);
+	Bureaucrat &operator=(const Bureaucrat &src);
+	~Bureaucrat();
+
+	int getGrade() const;
+	const std::string& getName() const;
+
+	void incrementGrade();
+	void decrementGrade();
+	void signForm(AForm& form);
+	void executeForm(AForm const & form) const;
+
+	class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+		
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+};
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& src);
+
+#endif
