2a159c6d43a6f72343abcaa43c57dad2cb0bdc19
Christian Andresen <christian@Mac.fritz.box>
2025-07-06 22:39:51 +0200

    new presidential class

---
+++ ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.cpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: christian <christian@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 03:51:59 by christian         #+#    #+#             */
+/*   Updated: 2025/07/06 19:09:38 by christian        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PresidentialPardonForm.hpp"
+
+// default constructor
+PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialPardonForm", 25, 5), target("default")
+{}
+
+// parameter constructor
+PresidentialPardonForm::PresidentialPardonForm(const std::string& target) 
+	: AForm("PresidentialPardonForm", 25, 5), target(target)
+{}
+
+// copy constructor
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other)
+	: AForm(other), target(other.target)
+{}
+
+// assignment operator
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& other)
+{
+	if (this != &other)
+	{
+		AForm::operator=(other);
+		target = other.target;
+	}
+	return *this;
+}
+
+// destructor
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+	std::cout << "PresidentialPardonForm destructor called" << std::endl;
+}
+
+// function that calls parent execute for checks
+void PresidentialPardonForm::execute(Bureaucrat const & executor) const
+{
+	AForm::execute(executor);
+}
+
+// action implementation
+void PresidentialPardonForm::executeAction() const
+{
+	std::cout << target << " has been pardoned by Zaphod Beeblebrox" << std::endl;
+}
---
+++ ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: christian <christian@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 03:52:02 by christian         #+#    #+#             */
+/*   Updated: 2025/07/03 04:13:30 by christian        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+private:
+	std::string target;
+
+public:
+	PresidentialPardonForm();
+	PresidentialPardonForm(const std::string& target);
+	PresidentialPardonForm(const PresidentialPardonForm& other);
+	PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
+	~PresidentialPardonForm();
+
+	void execute(Bureaucrat const & executor) const override;
+
+protected:
+	void executeAction() const override;
+};
+
+#endif
